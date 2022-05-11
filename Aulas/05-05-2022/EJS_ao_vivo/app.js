d5d61896bf2b182d0c6c4fb4a3039c51c6e4997a
Antonio Augusto Diniz Sousa <antonio.diniz.ext@dasa.com.br>
2022-05-10 21:39:05 -0300

    Alterações feitas durante a aula do dia 10/05/2022

--- Aulas/05-05-2022/EJS_ao_vivo/app.js
+++ Aulas/05-05-2022/EJS_ao_vivo/app.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const ProductsRoutes = require('./routes/ProductsRoutes')
+const path = require('path')
 
 const app = express()
 
 app.set('view engine', 'ejs')
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json())
 
 app.use('/products', ProductsRoutes)
